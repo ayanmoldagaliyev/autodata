dd8946738ea36641f5232509ffc5cddfcd16bca6
Ayan <ayan.moldagaliev159@gmail.com>
2021-09-18 11:44:43 +0600

    commit

--- routes/autodata.js
+++ routes/autodata.js
@@ -36,17 +36,4 @@ router.post('/create', async (req, res) => {
     res.redirect('/')
 })
 
-router.post('/embrace', async (req, res) => {
-    console.log(res.body.id)
-    const embrace = await Embrace.findById(res.body.id)
-    embrace = new Embrace({
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price
-    })
-    await embrace.save()
-    res.redirect('/embrace')
-})
-
 module.exports = router
